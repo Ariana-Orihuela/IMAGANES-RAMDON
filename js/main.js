8350d44204adfbe4f5c0f25aeb9b81e8878188e2
Ariana Orihuela Guevara <orihuelaguevaraariana@gmail.com>
2022-11-05 23:29:50 -0500

    primero

---
+++ js/main.js
@@ -0,0 +1,36 @@
+let imagenes = ['img/img1.jpg','img/img2.jpg','img/img3.jpg','img/img4.jpg', 'img/img5.jpg','img/img6.jpg','img/img7.jpg', 'img/img8.jpg'];
+let contador = 0;
+
+function carrousel(direccion){
+let direccion1 = direccion;
+
+if(direccion1=='atras'){
+  if(contador==0){
+    document.getElementById('imagen').src= imagenes[imagenes.length-1];
+    contador++;
+  }else if (contador<imagenes.length-1) {
+    document.getElementById('imagen').src= imagenes[imagenes.length-1-contador];
+    contador++;
+
+  }else {
+    document.getElementById('imagen').src= imagenes[0];
+    contador=0;
+  }
+}
+
+if(direccion1=='adelante'){
+  if(contador==0){
+    document.getElementById('imagen').src= imagenes[0];
+    contador++;
+  }else if (contador<imagenes.length-1) {
+    document.getElementById('imagen').src= imagenes[contador];
+    contador++;
+
+  }else {
+    document.getElementById('imagen').src= imagenes[imagenes.length-1];
+    contador=0;
+  }
+}
+
+}
+
